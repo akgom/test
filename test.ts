d8ef07b3ce10319999cb88f73cb7dc9335376659
akgom <132290469+akgom@users.noreply.github.com>
2026-03-23 20:29:46 -0400

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,29 @@
 roversaPet.setUltrasonicPins(DigitalPin.P13, DigitalPin.P14)
 roversaPet.setStopDistance(15)
 roversaPet.setCautionDistance(25)
+roversaPet.setPetStats(70, 20, 80)
+
+input.onButtonPressed(Button.A, function () {
+    roversaPet.petRobot()
+})
+
+input.onButtonPressed(Button.B, function () {
+    roversaPet.feedRobot()
+})
+
+input.onButtonPressed(Button.AB, function () {
+    roversaPet.showDistance()
+})
 
 basic.forever(function () {
-    roversaPet.updateFaceFromDistance()
+    roversaPet.updatePetState()
+    roversaPet.updateFace()
 
     if (roversaPet.dangerDetected()) {
         Roversa.stop()
     } else {
         Roversa.driveForward()
     }
+
+    basic.pause(500)
 })
